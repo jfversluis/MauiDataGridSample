f10c626edacd38eeb329a8b3fc6487a925d2849f
Gerald Versluis <gerald.versluis@microsoft.com>
2023-06-01 11:49:44 +0200

    Add Monkeys

--- MauiDataGridSample/MainPage.xaml.cs
+++ MauiDataGridSample/MainPage.xaml.cs
@@ -1,12 +1,28 @@
-﻿namespace MauiDataGridSample
+﻿using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace MauiDataGridSample
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+        private readonly HttpClient httpClient = new();
+
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+
+            Task.Run(async () =>
+            {
+                var monkeys = await httpClient.GetFromJsonAsync<Monkey[]>("https://montemagno.com/monkeys.json");
+
+                foreach(Monkey monkey in monkeys)
+                {
+                    Monkeys.Add(monkey);
+                }
+            });
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
